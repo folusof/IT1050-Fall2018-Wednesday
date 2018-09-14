57419bb63c2248d62f051d9932e45301fcadf8ae
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-09-13 23:17:08 -0400

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            // This is a comment
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.Write("World!");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello ");
+            Console.Write("World!");
+            Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+            Console.ReadLine();
+
+            Console.WriteLine("Hello ");
+            Console.WriteLine("World!");
+            Console.ReadLine();
+        }
+    }
+}
+
