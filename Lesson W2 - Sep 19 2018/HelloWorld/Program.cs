8767ebd938ca91ce906172804c4a3aecef479244
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-09-19 20:54:43 -0400

    Add files via upload

---
+++ Lesson W2 - Sep 19 2018/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string myName = "Howard Dayton";
+
+            Console.WriteLine("Hello ");
+            Console.Write("World! " + myName);
+            Console.ReadLine();
+            */
+
+            Student firstStudent = new Student();
+            Student secondStudent = new Student();
+            Student thirdStudent = new Student();
+
+            firstStudent.studentName = "Howard";
+            secondStudent.studentName = "Anthony";
+            thirdStudent.studentName = "Toby";
+
+            Console.WriteLine("Hello, my name is " + firstStudent.studentName );
+            Console.WriteLine("Hello, my name is " + secondStudent.studentName);
+            Console.WriteLine("Hello, my name is " + thirdStudent.studentName);
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+    
+    
+}
