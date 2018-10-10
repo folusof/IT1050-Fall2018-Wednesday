82a8322dc3a5e383b3a5c2c4e077d3c1c402dc8f
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-10-10 19:02:41 -0400

    Add files via upload

---
+++ Lesson W4 - Oct 3 2018/InClassApp4/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassApp4
+{
+    class Car
+    {
+        public string make;
+        public string Make 
+        {
+            get {return make;}
+            set { make = value; }
+        }
+
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = value;}
+        }
+
+        private string color;
+
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value;}
+        }
+
+        public void SpeedUp()
+        {
+            speed = speed + 10;
+        }
+
+        public void SlowDown()
+        {
+            speed = speed - 10;
+        }
+    }
+}
---
+++ Lesson W4 - Oct 3 2018/InClassApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car();
+
+            car1.Make = "Ford";
+            car1.Model = "Mustang";
+            car2.Make = "Chevy";
+            car2.Model = "Camaro";
+
+            car1.Speed = 0;
+            
+            Console.WriteLine("The make of the first car is {0} {1}", car1.Make, car1.Model); 
+            Console.WriteLine("The make of the first car is {0} {1}", car2.Make, car1.Model);
+
+            Console.WriteLine("The speed is {0}", car1.Speed);
+            car1.SpeedUp();
+            Console.WriteLine("The speed is {0}", car1.Speed);
+            car1.SpeedUp();
+            car1.SpeedUp();
+            Console.WriteLine("The speed is {0}", car1.Speed);
+
+            car1.SlowDown();
+            Console.WriteLine("The speed is {0}", car1.Speed);
+
+        }
+    }
+}
