5edb6ad3f72d060d2539225332a465764d135ca6
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-10-17 20:45:10 -0400

    Add files via upload

---
+++ Lesson W6 - Oct 17 2018/InClass-LabW6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass_LabW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int speedLimit;
+            int speed;
+
+            speedLimit = 35;
+            speed = 42;
+
+            if (speed > speedLimit)
+            {
+                Console.WriteLine("SLOW DOWN");
+            }
+            
+
+            int i;
+
+            i = 30;
+            while (i >= 10)
+            {
+                Console.WriteLine(i);
+                i = i - 2;
+            }
+            */
+
+            int numValue;
+            int exponentValue;
+            int resultValue;
+            int i;
+
+            Console.WriteLine("Enter the number value: ");
+            numValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the exponent value: ");
+            exponentValue = int.Parse(Console.ReadLine());
+
+            i = 1;
+            resultValue = numValue;
+
+            while (i < exponentValue)
+            {
+                resultValue = resultValue * numValue;
+                i = i + 1;
+            }
+            Console.WriteLine("The result is = {0}", resultValue);
+
+
+
+
+
+        }
+    }
+}
