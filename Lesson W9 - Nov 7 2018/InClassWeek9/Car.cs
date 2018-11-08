0a111c233d64053e4a63ee5e058b2ef8ffd366cc
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-11-07 20:57:44 -0500

    Add files via upload

---
+++ Lesson W9 - Nov 7 2018/InClassWeek9/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek9
+{
+    class Car
+    {
+        public  string  make;
+        public  string  model;
+        public  int     year;
+        public  string  color;
+        
+        private int     speed;
+        private int     topSpeed;
+
+        public void setTopSpeed(int pTopSpeed)
+        {
+            this.topSpeed = pTopSpeed;
+        }
+
+        public int getSpeed()
+        {
+            return speed;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("Honk Honk");
+        }
+
+        public void Beep()
+        {
+            Console.WriteLine("Beep Beep");
+        }
+
+
+        public void speedUp()
+        {
+            if (speed < topSpeed)
+            {
+                speed++;
+            }
+            Console.WriteLine("Speeding Up....");
+        }
+
+        public void slowDown()
+        {
+            if (speed > 0)
+            {
+                speed--;
+            }
+            Console.WriteLine("Slowing Down....");
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("I'm a " + make + " " + model + " and I'm going " + speed + "mph");
+        }
+    }
+}
---
+++ Lesson W9 - Nov 7 2018/InClassWeek9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Car myFirstCar = new Car();
+            Car mySecondCar = new Car();
+            Car myThirdCar = new Car();
+
+            myFirstCar.make = "Chevy";
+            myFirstCar.model = "Camaro";
+            //myFirstCar.topSpeed = 280;
+            myFirstCar.setTopSpeed(280);
+//            myFirstCar.speedUp();
+//            myFirstCar.speedUp();
+            //Console.WriteLine(myFirstCar.getSpeed());
+            for (int i = 0; i <= 200; i++)
+            {
+                myFirstCar.speedUp();
+            }
+                myFirstCar.ToString();
+
+        }
+    }
+}
