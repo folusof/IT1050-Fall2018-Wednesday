38710c851a1d44653f2803372f2c2cf0004c3681
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-11-14 20:50:04 -0500

    Add files via upload

---
+++ Lesson W10 - Nov 14 2018/InClassWeek10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SuperHero SuperHero1 = new SuperHero();
+            SuperHero SuperHero2 = new SuperHero();
+            SuperHero SuperHero3 = new SuperHero();
+
+            SuperHero1.setName("Spiderman");
+            SuperHero2.setName("Superman");
+            SuperHero3.setName("Wonder Woman");
+
+            SuperHero1.PowerUp(100, false);
+            SuperHero2.PowerUp(100, true);
+            SuperHero3.PowerUp(200, true);
+
+            Console.WriteLine(SuperHero1.getName() + " has a skill level of " + SuperHero1.skillLevel );
+            Console.WriteLine(SuperHero2.getName() + " has a skill level of " + SuperHero2.skillLevel);
+            Console.WriteLine(SuperHero3.getName() + " has a skill level of " + SuperHero3.skillLevel);
+        }
+    }
+}
---
+++ Lesson W10 - Nov 14 2018/InClassWeek10/SuperHero.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek10
+{
+    class SuperHero
+    {
+        private     string		Name;
+        public      string		Power;
+        public      int         skillLevel;
+        public      int         Health;
+        private     string      AlterEgo;
+        public      string      Alignment;
+        public      string      Weakness;
+        private     int         Age;
+        public      string      Gender;
+        public      string      Sidekicks;
+
+        public void setName(string pName) 
+        {
+            this.Name = pName;
+        }
+
+        public string getName()
+        {
+            return this.Name;
+        }
+
+
+        public 	string 	Attack(string target)
+        {
+            return "";
+        }
+
+        public 	string	Defense (string target)
+        {
+            return "";
+        }
+
+        public 	int	PowerUp(int amount, bool boost)
+        {
+            this.skillLevel = this.skillLevel + amount;
+
+            if (boost == true)
+            {
+                this.skillLevel = this.skillLevel + amount;
+            }
+
+            return 0;
+        }
+
+        public int heal(int amount, int duration)
+        {
+
+            return 0;
+        }
+
+
+    }
+}
