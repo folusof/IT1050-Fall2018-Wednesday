07cec6b9da742fec990ef3c18b50f95eb0d1a1aa
Foluso Fayomi <foluso.fayomi@tri-c.edu>
2018-12-05 20:56:31 -0500

    Add files via upload

---
+++ Lesson W13 - Dec 5 2018/InClassWeek13/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek13
+{
+    class Car
+    {
+        private string  Make;
+        private string  Model;
+        private int     Year;
+        private int     Speed;
+        private string  Color;
+        private string  Transmission;
+        private int     Mileage;
+
+        public Car(string pMake, string pModel)
+        {
+            this.Make = pMake;
+            this.Model = pModel;
+        }
+
+        public Car(string pMake, string pModel, int pYear, string pColor)
+        {
+            this.Make = pMake;
+            this.Model = pModel;
+            this.Year = pYear;
+            this.Color = pColor;
+        }
+
+        public Car(string pMake, int pYear)
+        {
+            this.Make = pMake;
+            this.Year = pYear;
+        }
+
+        public void Display1()
+        {
+            Console.WriteLine("My " + Year + " " + this.Make + " " + this.Model + " is " + this.Color);
+        }
+
+        public string Display2()
+        {
+            string sDisplay;
+
+            sDisplay = "My " + Year + " " + this.Make + " " + this.Model + " is " + this.Color;
+            return sDisplay;
+        }
+
+        public void Display3()
+        {
+            Console.WriteLine("My " + Year + " " + this.Make + " " + this.Model + 
+                                " is going " + this.Speed + " MPH.");
+        }
+
+        public void SpeedUp()
+        {
+            this.Speed++;
+        }
+
+    }
+}
---
+++ Lesson W13 - Dec 5 2018/InClassWeek13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassWeek13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Ford", "Mustang");
+
+            Car car2 = new Car("Chevy", "Camaro", 1999, "Red");
+
+            //car2.Display1();
+
+            Console.WriteLine( car2.Display2() );
+
+            for (int i = 0; i < 60; i++)
+            {
+                car2.SpeedUp();
+            }
+
+            car2.Display3();
+
+        }
+    }
+}
